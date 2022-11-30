a18138ea177e59b1121cbe502c7ad2eb9d0fcfbd
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-11-30 16:50:15 +0300

    Remove D3 Chart

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import './state';
-import renderD3Chart from './charts/d3/exampleChart';
 import renderEChart from './core/ExampleChart';
 import renderTable from './core/ExampleTable';
 import './styles/styles.css';
@@ -8,7 +7,6 @@ import './styles/styles.css';
  * Run your code after the page has loaded
  */
 window.addEventListener('load', () => {
-  renderD3Chart();
   renderEChart();
   renderTable('dicharts--table-example');
 });
