09407198ce8c855550d5186bf6834e4e1763a56c
Edwin P. Magezi <edwinm_p@yahoo.com>
2022-12-01 11:51:24 +0300

    Fetch KeyFacts data from state URL

--- src/core/KeyFacts.js
+++ src/core/KeyFacts.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import fetchCSVData from '../utils/data';
 import KeyFacts from './components/KeyFacts';
+import NoDataCentered from './components/NoDataCentered';
 
 /**
  * Run your code after the page has loaded
@@ -13,12 +15,50 @@ const renderViz = (className) => {
         Array.prototype.forEach.call(chartNodes, (chartNode) => {
           const dichart = new window.DICharts.Chart(chartNode.parentElement);
           // const filterWrapper = addFilterWrapper(chartNode);
+          dichart.showLoading();
+          if (window.DIState) {
+            const root = createRoot(chartNode);
+            window.DIState.addListener(() => {
+              dichart.showLoading();
+              const { keyFacts, location } = window.DIState.getState;
 
-          // create dropdowns
-          const root = createRoot(chartNode);
-          root.render(<KeyFacts />);
+              if (keyFacts && keyFacts.url) {
+                if (keyFacts.url.endsWith('.csv')) {
+                  fetchCSVData(keyFacts.url).then((data) => {
+                    root.render(<KeyFacts data={data} location={location} />);
+                  });
+                } else {
+                  window
+                    .fetch(keyFacts.url)
+                    .then((response) => response.json())
+                    .then((data) => {
+                      if (Array.isArray(data)) {
+                        window.console.log(data);
+                        root.render(<KeyFacts data={data} location={location} />);
+                      } else if (data.results) {
+                        root.render(<KeyFacts data={data.results} location={location} />);
+                      } else {
+                        window.console.error(
+                          'Invalid data shape. Expected an array or an object with a results property.',
+                          data
+                        );
+                        root.render(<NoDataCentered />);
+                      }
+                    })
+                    .catch((error) => {
+                      window.console.log(error);
+                      root.render(<NoDataCentered />);
+                    });
+                }
+              } else {
+                root.render(<NoDataCentered />);
+              }
 
-          dichart.hideLoading();
+              dichart.hideLoading();
+            });
+          } else {
+            window.console.log('State is not defined');
+          }
         });
       },
     },
