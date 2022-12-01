db55ee7ab842d1b501c7db7f30ae992a604addab
ThatcherK <kaliithatmau@gmail.com>
2022-12-01 12:07:41 +0300

    Remove csv fetching provision

--- src/components/SelectorDropdowns.js
+++ src/components/SelectorDropdowns.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { fetchData } from '../utils/data';
 import { addFilterWrapper } from '../widgets/filters';
 import Select from './Select';
 import ChartFilters from './ChartFilters';
@@ -28,49 +27,47 @@ const init = (className) => {
            *
            * const chart = window.echarts.init(chartNode);
            */
-          fetchData(DATAFILE).then((data) => {
-            const processedData = Array.from(new Set(data.map((d) => d.SubCounty)));
-            const selectorFilterWrapper = addFilterWrapper(chartNode);
+          const processedData = Array.from(new Set(DATAFILE.map((d) => d.SubCounty)));
+          const selectorFilterWrapper = addFilterWrapper(chartNode);
 
-            // Create dropdowns
-            const rootElement = createRoot(selectorFilterWrapper);
-            rootElement.render(
-              <ChartFilters selectErrorMessage={selectErrorMessage}>
-                <Select
-                  label="Select Subcounty"
-                  options={processedData.map((d) => ({ value: d, label: d }))}
-                  classNamePrefix="subcounty-filter sticky-top"
-                  isClearable={false}
-                  defaultValue={[{ value: 'Wakiso', label: 'Wakiso', isCloseable: true }]}
-                  onChange={(item) => {
-                    window.DIState.setState({ subCounty: item.value });
-                  }}
-                  css={{ minWidth: '200px' }}
-                />
-                <Select
-                  label="Select School Level"
-                  options={[
-                    { value: 'primary', label: 'Primary' },
-                    { value: 'secondary', label: 'Secondary' },
-                  ]}
-                  classNamePrefix="level-filter sticky-top"
-                  isClearable={false}
-                  defaultValue={[{ value: 'primary', label: 'Primary', isCloseable: true }]}
-                  onChange={(item) => {
-                    window.DIState.setState({ level: item.value });
-                  }}
-                  css={{ minWidth: '200px' }}
-                />
-              </ChartFilters>
-            );
-            if (window.DIState) {
-              window.DIState.setState({ subCounty: 'United States' });
-              window.DIState.setState({ level: 'primary' });
-            }
+          // Create dropdowns
+          const rootElement = createRoot(selectorFilterWrapper);
+          rootElement.render(
+            <ChartFilters selectErrorMessage={selectErrorMessage}>
+              <Select
+                label="Select Subcounty"
+                options={processedData.map((d) => ({ value: d, label: d }))}
+                classNamePrefix="subcounty-filter sticky-top"
+                isClearable={false}
+                defaultValue={[{ value: 'Wakiso', label: 'Wakiso', isCloseable: true }]}
+                onChange={(item) => {
+                  window.DIState.setState({ subCounty: item.value });
+                }}
+                css={{ minWidth: '200px' }}
+              />
+              <Select
+                label="Select School Level"
+                options={[
+                  { value: 'primary', label: 'Primary' },
+                  { value: 'secondary', label: 'Secondary' },
+                ]}
+                classNamePrefix="level-filter sticky-top"
+                isClearable={false}
+                defaultValue={[{ value: 'primary', label: 'Primary', isCloseable: true }]}
+                onChange={(item) => {
+                  window.DIState.setState({ level: item.value });
+                }}
+                css={{ minWidth: '200px' }}
+              />
+            </ChartFilters>
+          );
+          if (window.DIState) {
+            window.DIState.setState({ subCounty: 'United States' });
+            window.DIState.setState({ level: 'primary' });
+          }
 
-            dichart.hideLoading();
-            chartNode.parentElement.classList.add('auto-height');
-          });
+          dichart.hideLoading();
+          chartNode.parentElement.classList.add('auto-height');
         });
       },
     },
