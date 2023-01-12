6d8c5713cdd681a0d0381cd89cbeb46292d79303
David Ebukali <debukali@gmail.com>
2023-01-12 14:43:52 +0300

    Render legend in the sidebar

--- src/core/components/DistrictMapSidebar.js
+++ src/core/components/DistrictMapSidebar.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ErrorBoundary from '../../components/ErrorBoundary';
 import DistrictMapFilters from './DistrictMapFilters';
+import Legend from './Legend/Legend';
+import LegendItem from './Legend/LegendItem';
+import { renderLegendItems } from '../../components/BaseMap/utils';
 
 const DistrictMapSidebar = () => (
   <div className="spotlight__content">
     <ErrorBoundary>
       <DistrictMapFilters />
     </ErrorBoundary>
+    <Legend>
+      {renderLegendItems([5, 10, 15, 20], ['#faa2c1', '#f06595', '#d6336c', '#c2255c', '#a61e4d'])}
+      <LegendItem>no data / not applicable</LegendItem>
+    </Legend>
   </div>
 );
 
