e48728c06a98ba5ed145574a36f2ab0794d35b07
Edwin P. Magezi <edwinm_p@yahoo.com>
2023-01-24 20:01:51 +0300

    Update legend on indicator change

--- src/core/components/DistrictMapSidebar.js
+++ src/core/components/DistrictMapSidebar.js
@@ -1,23 +1,42 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import ErrorBoundary from '../../components/ErrorBoundary';
+import { DistrictMapContext } from '../context';
 import DistrictMapFilters from './DistrictMapFilters';
 import Legend from './Legend/Legend';
 import LegendItem from './Legend/LegendItem';
-import { renderLegendItems } from '../../components/BaseMap/utils';
+
+export const renderLegendItems = (range, colours) => {
+  if (range && colours) {
+    return range
+      .map((rnge, index) => (
+        <LegendItem className={`item-${rnge}`} bgColor={colours[index]} key={index}>
+          {index === 0 ? `< ${range[0]}` : `${range[index - 1]}-${rnge}`}
+        </LegendItem>
+      ))
+      .concat([
+        <LegendItem className={`item-last`} bgColor={colours[colours.length - 1]} key={range.length}>
+          {`> ${range[range.length - 1]}`}
+        </LegendItem>,
+      ]);
+  }
+
+  return null;
+};
 
 const DistrictMapSidebar = () => {
-  const { map } = window.DIState.getState;
-  const range = map.aggregator === 'sum' ? map.rangeSum : map.rangeAvg;
+  const { activeIndicator } = useContext(DistrictMapContext);
 
   return (
     <div className="spotlight__content">
       <ErrorBoundary>
         <DistrictMapFilters />
       </ErrorBoundary>
-      <Legend>
-        {renderLegendItems(range, map.colours)}
-        <LegendItem>no data / not applicable</LegendItem>
-      </Legend>
+      {activeIndicator ? (
+        <Legend>
+          {renderLegendItems(activeIndicator.range, activeIndicator.colours)}
+          <LegendItem>no data / not applicable</LegendItem>
+        </Legend>
+      ) : null}
     </div>
   );
 };
