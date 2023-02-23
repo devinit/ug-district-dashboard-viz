1f95b5e85e07e15f51f7249e719537dc31634b3b
David Ebukali <debukali@gmail.com>
2023-02-23 15:38:22 +0300

    Revert selectors to render only at the page top

--- src/core/SelectorDropdowns.js
+++ src/core/SelectorDropdowns.js
@@ -4,7 +4,7 @@ import { createRoot } from 'react-dom/client';
 import { addFilterWrapper } from '../widgets/filters';
 import Selectors from './components/Selectors';
 
-const renderSelectors = (className, id) => {
+const renderSelectors = (className) => {
   window.DICharts.handler.addChart({
     className,
     d3: {
@@ -34,7 +34,7 @@ const renderSelectors = (className, id) => {
                   'Invalid value for selectors - an Array is expected. Please review the documentation!'
                 );
               }
-              rootElement.render(<Selectors configs={selectors} renderIds={id} />);
+              rootElement.render(<Selectors configs={selectors} />);
             });
           } else {
             window.console.log('State is not defined');
