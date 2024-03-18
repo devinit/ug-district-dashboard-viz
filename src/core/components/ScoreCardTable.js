3e86c2d3293fd3d6b1e746c99b103f43b7c0c1ae
ThatcherK <kaliithatmau@gmail.com>
2024-03-18 12:24:10 +0300

    Add table legend

--- src/core/components/ScoreCardTable.js
+++ src/core/components/ScoreCardTable.js
@@ -1,10 +1,44 @@
 /** @jsx jsx */
 import { jsx } from '@emotion/react';
+import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import Table from '../../components/Table';
 import { getTableCellColor } from '../utils/tables';
 
 const ScoreCardTable = (props) => {
+  const scoreCardLegendData = [
+    { color: '#3b8c62', caption: 'On target' },
+    { color: '#cd2b2a', caption: 'Below target' },
+    { color: '#cdcfd1', caption: 'No data' }
+  ];
+  useEffect(() => {
+    const legendContainer = document.createElement('div');
+    const legendContent = `${scoreCardLegendData
+      .map(
+        (item) =>
+          `<span>
+            <i style="background:${item.color};border-radius:1px;width:40px;"></i>
+            <label>${item.caption}</label>
+          </span>`
+      )
+      .join('')}`;
+    legendContainer.classList.add('custom-legend', 'score-table-legend');
+    legendContainer.innerHTML = legendContent;
+    const tableContainer = document.querySelector('.score-table-container');
+    const { firstChild } = tableContainer;
+    if (firstChild) {
+      tableContainer.insertBefore(legendContainer, firstChild);
+    } else {
+      tableContainer.appendChild(legendContainer);
+    }
+
+    return () => {
+      if (tableContainer) {
+        tableContainer.removeChild(legendContainer);
+      }
+    };
+  });
+
   const renderRows = (rows, header = false) =>
     rows.map((row, index) => (
       <tr key={index}>
@@ -15,33 +49,35 @@ const ScoreCardTable = (props) => {
             <td
               css={{
                 backgroundColor: getTableCellColor(cell, props.thresholds),
-                color: getTableCellColor(cell, props.thresholds) ? 'white' : 'black',
+                color: getTableCellColor(cell, props.thresholds) ? 'white' : 'black'
               }}
               key={key}
             >
-              {cell}
+              {Number(cell) === 0 ? '' : cell}
             </td>
-          ),
+          )
         )}
       </tr>
     ));
 
   return (
-    <Table>
-      <thead>
-        {renderRows(
-          props.rows.filter((row, index) => index === 0),
-          true,
-        )}
-      </thead>
-      <tbody>{renderRows(props.rows.filter((row, index) => index > 0))}</tbody>
-    </Table>
+    <div className="score-table-container">
+      <Table>
+        <thead>
+          {renderRows(
+            props.rows.filter((row, index) => index === 0),
+            true
+          )}
+        </thead>
+        <tbody>{renderRows(props.rows.filter((row, index) => index > 0))}</tbody>
+      </Table>
+    </div>
   );
 };
 
 ScoreCardTable.propTypes = {
   rows: PropTypes.arrayOf(PropTypes.array),
-  thresholds: PropTypes.object,
+  thresholds: PropTypes.object
 };
 
 export default ScoreCardTable;
