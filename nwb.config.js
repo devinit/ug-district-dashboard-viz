c80af2ad616ee3d0b16f9b85ca5cd0c1fa6ead03
David <debukali@gmail.com>
2025-02-17 10:56:28 +0300

    Refactor Babel configuration and update dependencies for improved compatibility

--- nwb.config.js
+++ nwb.config.js
@@ -8,23 +8,43 @@ module.exports = {
           test: '*.css',
           // Configuration options for css-loader
           css: {
-            modules: false,
-          },
-        },
-      ],
+            modules: false
+          }
+        }
+      ]
     },
     extractCSS: {
-      filename: process.env.NODE_ENV === 'production' ? '[name].[contenthash:8].css' : '[name].css',
+      filename: process.env.NODE_ENV === 'production' ? '[name].[contenthash:8].css' : '[name].css'
     },
     extra: {
       // mode: 'production',
       externals: {
-        d3: 'd3',
+        d3: 'd3'
       },
       devtool: process.env.NODE_ENV === 'production' ? 'none' : 'source-map',
-    },
+      module: {
+        rules: [
+          {
+            test: /\.js$/,
+            include: /node_modules[\\/]mapbox-gl/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env'],
+                plugins: [
+                  '@babel/plugin-proposal-class-properties',
+                  '@babel/plugin-proposal-optional-chaining',
+                  '@babel/plugin-proposal-nullish-coalescing-operator'
+                ]
+              }
+            }
+          }
+        ]
+      }
+    }
   },
   babel: {
     presets: ['@babel/preset-react'],
-  },
+    plugins: ['@babel/plugin-proposal-class-properties']
+  }
 };
