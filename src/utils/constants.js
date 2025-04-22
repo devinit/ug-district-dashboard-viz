c543e9d83d98f1d975eb8a95b1a813e01a2f9987
David <debukali@gmail.com>
2025-04-22 21:46:02 +0300

    Remove enrollment urls from constants.js file

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,10 +1,2 @@
 // eslint-disable-next-line import/prefer-default-export
 export const NO_DATA = 0;
-export const MASINDI_SECONDARY_ENROLLMENT_URL =
-  'https://raw.githubusercontent.com/devinit/ug-district-dashboard-viz/dev/public/assets/data/masindi/secondary_school_enrolment.csv';
-export const MASINDI_PRIMARY_ENROLLMENT_URL =
-  'https://raw.githubusercontent.com/devinit/ug-district-dashboard-viz/dev/public/assets/data/masindi/primary_school_enrolment.csv';
-export const KAYUNGA_SECONDARY_ENROLLMENT_URL =
-  'https://raw.githubusercontent.com/devinit/ug-district-dashboard-viz/dev/public/assets/data/kayunga/learners_enrolment_secondary.csv';
-export const KAYUNGA_PRIMARY_ENROLLMENT_URL =
-  'https://raw.githubusercontent.com/devinit/ug-district-dashboard-viz/dev/public/assets/data/kayunga/primary_and_kindergaten_enrolment.csv';
