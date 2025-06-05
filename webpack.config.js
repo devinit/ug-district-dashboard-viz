a631724aae1e337e0be9176fa03d46e6f98cda98
David <debukali@gmail.com>
2025-06-05 16:40:31 +0300

    Remove unnecessary Babel plugins included in preset-env

--- webpack.config.js
+++ webpack.config.js
@@ -24,12 +24,7 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
-            plugins: [
-              '@babel/plugin-transform-class-properties',
-              '@babel/plugin-transform-optional-chaining',
-              '@babel/plugin-transform-nullish-coalescing-operator'
-            ]
+            presets: ['@babel/preset-env', '@babel/preset-react']
           }
         }
       },
